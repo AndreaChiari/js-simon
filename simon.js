9466478834f748b81d537a21f37ffdd071f7c5ab
AndreaChiari <chiariandrea94@gmail.com>
2022-12-20 14:29:07 +0100

    add-time-value

--- simon.js
+++ simon.js
@@ -8,4 +8,23 @@ const counterDays = document.getElementById('days')
 
 // stabilisco una variabile che mi faccia vedere la data del giorno di natale
 let xmasDate = new Date('Dec 25 , 2022 00:00:00').getTime();
-console.log(xmasDate)
+console.log(xmasDate);
+
+// stabilisco una variabile che mi faccia vedere la data del giorno di oggi
+let currentDate = new Date().getTime();
+console.log(currentDate);
+
+// stabilisco una variabile che mi faccia vedere la differenza in millisecondi tra le due date
+let timeDistance = xmasDate - currentDate
+console.log(timeDistance)
+
+// trovo il valore esatto dei secondi, minuti, ore e giorni con dei calcoli
+
+let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+  console.log(days,hours,minutes,seconds);
+
+
+
